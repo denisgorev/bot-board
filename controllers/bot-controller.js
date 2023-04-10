defa617011ea631062af6ab3b9930465d3ee3e1b
Денис Горев <denisgorev1992@gmail.com>
2023-04-10 23:28:03 +0200

    broadcastining bug fixed

--- controllers/bot-controller.js
+++ controllers/bot-controller.js
@@ -17,14 +17,15 @@ const boardGameBot = () => {
    
     ctx.replyWithHTML(
       `Привет, ${ctx.from.first_name}! Меня зовут Виктор, и я дилер в этом казино. Не хочешь сыграть?`
+    //   <A href="tg://user?id={числовой id}">link</a>`
     );
     let user = []
-        // console.log(ctx.from, ctx.chat.id)
+
     try {
         user = await User.find({
             chatId: ctx.chat.id
         })
-        console.log(user)
+
 
         if (user.length === 0) {
             // console.log(user)
@@ -72,19 +73,14 @@ const boardGameBot = () => {
       if (broadcast.length !==0) {
         let user_list  = users.map(user => user.chatId)
         for (i in user_list) {
-          console.log(i)
           bot.telegram.sendMessage(user_list[i], broadcast[0].text)
         }
       }
    
-      
-      return ctx.wizard.next();
+      return ctx.scene.leave();
+
     },
-    async (ctx) => {
-        // ctx.reply('Thank you for your replies, we ll contact your soon');
-        // console.log(ctx.wizard.state.data.code)
-        return ctx.scene.leave();
-    },
+ 
   );
 
   const stage = new Scenes.Stage([contactDataWizard]);
@@ -115,12 +111,6 @@ bot.use(session());
   });
   
 
-  // bot.on('text', ctx => {
-  //     let btc_address = ctx.message.text
-  //     ctx.replyWithHTML(
-  //         btc_address
-  //       )
-  // })
   bot.launch(); // запуск бота
 };
 
