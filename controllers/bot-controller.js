feecc6e3e82bb5bebdf0641e89a48b4922b62a19
Денис Горев <denisgorev1992@gmail.com>
2023-04-27 20:22:22 +0200

    added deletion of all users, wake up function

--- controllers/bot-controller.js
+++ controllers/bot-controller.js
@@ -1,49 +1,49 @@
 const { Telegraf } = require("telegraf");
 
-const { Scenes, Stage, session } = require ('telegraf');
+const { Scenes, Stage, session } = require("telegraf");
 const Broadcast = require("../models/broadcast");
 
-
 const Script = require("../models/script");
-const User = require('../models/user');
-const {yesNoKeyboard} = require("../utils/keyboards");
-
+const User = require("../models/user");
+const { yesNoKeyboard } = require("../utils/keyboards");
 
 const bot = new Telegraf(process.env.TOKEN);
 
-
-
-
-
 const boardGameBot = () => {
   bot.start(async (ctx) => {
-   
     ctx.replyWithHTML(
       `Привет, ${ctx.from.first_name}! Меня зовут Виктор, и я дилер в этом казино. Не хочешь сыграть?`
-      
-    //   <A href="tg://user?id={числовой id}">link</a>`
+
+      //   <A href="tg://user?id={числовой id}">link</a>`
     );
 
-    let user = []
+    let user = [];
 
     try {
-        user = await User.find({
-            chatId: ctx.chat.id
-        })
+      user = await User.find({
+        chatId: ctx.chat.id,
+      });
+      // console.log(ctx.from)
 
-
-        if (user.length === 0) {
-            // console.log(user)
-            let newUser = new User ({
-                name: ctx.from.first_name,
-                chatId: ctx.chat.id
-            })
-            await newUser.save()
-            
+      if (user.length === 0) {
+        if (ctx.from.username) {
+          console.log(ctx.from.username);
+          let newUser = new User({
+            name: ctx.from.username,
+            chatId: ctx.chat.id,
+          });
+          await newUser.save();
+        } else {
+          let newUser = new User({
+            name: ctx.from.first_name,
+            chatId: ctx.chat.id,
+          });
+          await newUser.save();
         }
-    } catch (err) {
-        console.log(err);
       }
+    } catch (err) {
+      console.log(err);
+    }
 
     // console.log(ctx.from, ctx.chat.id);
   });
@@ -51,62 +51,65 @@ const boardGameBot = () => {
   const contactDataWizard = new Scenes.WizardScene(
     "broadcasting",
     async (ctx) => {
-        let broadcasts;
-        try {
-          //code: input_code
-          broadcasts = await Broadcast.find({});
-        } catch (err) {
-          console.log(err);
-        }
-        let broadcastsCodes = []
-        broadcastsCodes = broadcasts.map(broadcast => broadcast.code)
-        
-      ctx.reply(`введите код команды для бродкаста, доступные коды: ${broadcastsCodes}`);
+      let broadcasts;
+      try {
+        //code: input_code
+        broadcasts = await Broadcast.find({});
+      } catch (err) {
+        console.log(err);
+      }
+      let broadcastsCodes = [];
+      broadcastsCodes = broadcasts.map((broadcast) => broadcast.code);
+
+      ctx.reply(
+        `введите код команды для бродкаста, доступные коды: ${broadcastsCodes}`
+      );
       ctx.wizard.state.data = {};
       return ctx.wizard.next();
     },
     async (ctx) => {
-        ctx.wizard.state.data.code = ctx.message.text;
-        let users;
-        let broadcast;
-        try {
-            users = await User.find({});
-            broadcast = await Broadcast.find({code: ctx.wizard.state.data.code})
-        } catch (err) {
-          console.log(err);
-        }
-      if (broadcast.length !==0) {
-        let user_list  = users.map(user => user.chatId)
+      ctx.wizard.state.data.code = ctx.message.text;
+      let users;
+      let broadcast;
+      try {
+        users = await User.find({});
+        broadcast = await Broadcast.find({ code: ctx.wizard.state.data.code });
+      } catch (err) {
+        console.log(err);
+      }
+      if (broadcast.length !== 0) {
+        let user_list = users.map((user) => user.chatId);
         for (i in user_list) {
-          bot.telegram.sendMessage(user_list[i], broadcast[0].text)
+          bot.telegram.sendMessage(user_list[i], broadcast[0].text);
         }
       }
-   
-      return ctx.scene.leave();
 
-    },
- 
+      return ctx.scene.leave();
+    }
   );
 
   const stage = new Scenes.Stage([contactDataWizard]);
 
-bot.use(session());
+  bot.use(session());
   bot.use(stage.middleware());
   bot.command("broadcasting", (ctx) => {
     ctx.scene.enter("broadcasting");
   });
 
-//   bot.hears('/broadcast', async (ctx) => {
-//     bot.telegram.sendMessage(306807986, 'крол привет')
-//   })
+  //   bot.hears('/broadcast', async (ctx) => {
+  //     bot.telegram.sendMessage(306807986, 'крол привет')
+  //   })
+
+  bot.command("delete_users", async (ctx) => {
+    try {
+      await User.deleteMany({});
+      ctx.replyWithHTML(`Я удалил всех игроков, я могу идти спать?`);
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
   bot.on("text", async (ctx) => {
-    // ctx.replyWithHTML(
-    //     `Вы действительно хотите добавить задачу:\n\n`+
-    //     `<i>${ctx.message.text}</i>`,
-    //     
-    // )
-
     let input_code = ctx.message.text;
     let script;
     try {
@@ -115,31 +118,29 @@ bot.use(session());
     } catch (err) {
       console.log(err);
     }
-    if (script.length !== 0) {    
-
-        ctx.reply(script[0].text, yesNoKeyboard(script[0].next_codes)); 
+    if (script.length !== 0) {
+      ctx.reply(script[0].text, yesNoKeyboard(script[0].next_codes));
     } else {
-        ctx.reply(`слушай, дружище, я такой команды не знаю, что еще за "${ctx.message.text}"`)
+      ctx.reply(
+        `слушай, дружище, я такой команды не знаю, что еще за "${ctx.message.text}"`
+      );
     }
-
   });
   bot.action(/.+/, async (ctx) => {
     // console.log('action')
     let input_code = ctx.callbackQuery.data;
     let script;
     try {
-
       script = await Script.find({ code: input_code });
     } catch (err) {
       console.log(err);
     }
-    if (script.length !== 0) {    
-        // console.log(script[0])
+    if (script.length !== 0) {
+      // console.log(script[0])
 
-        ctx.reply(script[0].text, yesNoKeyboard(script[0].next_codes)); 
+      ctx.reply(script[0].text, yesNoKeyboard(script[0].next_codes));
     }
-
-})
+  });
 
   bot.launch(); // запуск бота
 };
--- index.js
+++ index.js
@@ -4,10 +4,37 @@ const app = express();
 const PORT = process.env.PORT
 const boardGameBot = require('./controllers/bot-controller')
 const mongoose = require('mongoose');
+const axios = require('axios');
+
+// const WAIT_INTERVAL = 1680000 // 28 minutes
+const WAIT_INTERVAL = 10000
+//  const TIMEOUT = 28800000 //8 hours
+const TIMEOUT = 7200000
+let interval = 0;
 
 app.get('/', (req, res) => {
     res.send('Hello debug_Yourself')
 })
+
+
+
+
+const wakeUp = async () => {
+    try {
+        await axios.get('https://bot-boardgame.onrender.com')
+    } catch (err) {
+        console.log(err)
+    }
+};
+const timeFinish = () => {
+    clearInterval(interval);
+    console.log('go to sleep');
+}
+
+interval = setInterval(wakeUp, WAIT_INTERVAL);
+setTimeout(timeFinish, TIMEOUT);
+
+
 
 boardGameBot.boardGameBot()
 
